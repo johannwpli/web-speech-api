06fae88216f6e8d123e11e17f573c48f2a901511
paint1024 <paint1024@hotmail.com>
2020-04-29 14:37:48 +0800

    use textarea text to match voice

--- show-me-the-talk/phrase-matcher.js
+++ show-me-the-talk/phrase-matcher.js
@@ -3,20 +3,21 @@ var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
 
 var phrases = [
-  'People who have been exposed to the new coronavirus might practice self-quarantine.',
-  'This brand-new virus can be transmitted between humans through droplets and contact.',
-  'Canceling events that are likely to draw crowds is an example of social distancing.',
-  'People who contract the coronavirus might spread the disease before symptoms show.',
-  'We could take precautions to prevent infection, such as washing hands regularly.',
-  'The World Health Organization declares the COVID-19 outbreak a pandemic as it ravages the world.'
+  'People who have been exposed to the new coronavirus might practice self-quarantine',
+  'This brand-new virus can be transmitted between humans through droplets and contact',
+  'Canceling events that are likely to draw crowds is an example of social distancing',
+  'People who contract the coronavirus might spread the disease before symptoms show',
+  'We could take precautions to prevent infection, such as washing hands regularly',
+  'The World Health Organization declares the COVID-19 outbreak a pandemic as it ravages the world'
 ];
 
 var phrasePara = document.querySelector('.phrase');
 var resultPara = document.querySelector('.result');
-var diagnosticPara = document.querySelector('.output');
+var diagnoPara = document.querySelector('.output');
 
 var testBtn = document.querySelector('#test'); // Johann
 var recoBtn = document.querySelector('#reco'); // Johann
+var textAra = document.querySelector('#text'); // Johann
 
 function randomPhrase() {
   var number = Math.floor(Math.random() * phrases.length);
@@ -31,14 +32,17 @@ function testSpeechSet() {
   // To ensure case consistency while checking with the returned output text
   phrase = phrase.toLowerCase();
   phrasePara.textContent = phrase;
-  document.getElementById("text").value = phrase; // Johann
+  textAra.value = phrase; // Johann
   resultPara.textContent = 'Right or wrong?';
   resultPara.style.background = 'rgba(0,0,0,0.2)';
-  diagnosticPara.textContent = '...diagnostic messages';
+  diagnoPara.textContent = '...diagnostic messages';
 }
 
 function testSpeechGet() {
-  var phrase = document.getElementById("text").value; // Johann
+  textAra.value = textAra.value.toLowerCase();
+  var phrase = textAra.value; // Johann
+  phrase = phrase.toLowerCase(); // Johann
+  phrasePara.textContent = phrase; // Johann
   var grammar = '#JSGF V1.0; grammar phrase; public <phrase> = ' + phrase +';';
   var recognition = new SpeechRecognition();
   var speechRecognitionList = new SpeechGrammarList();
@@ -60,7 +64,7 @@ function testSpeechGet() {
     // The second [0] returns the SpeechRecognitionAlternative at position 0.
     // We then return the transcript property of the SpeechRecognitionAlternative object 
     var speechResult = event.results[0][0].transcript.toLowerCase();
-    diagnosticPara.textContent = 'Speech received: ' + speechResult + '.';
+    diagnoPara.textContent = 'Speech received: ' + speechResult + '.';
     if(speechResult === phrase) {
       resultPara.textContent = 'I heard the correct phrase!';
       resultPara.style.background = 'lime';
@@ -81,7 +85,7 @@ function testSpeechGet() {
   recognition.onerror = function(event) {
     testBtn.disabled = false;
     testBtn.textContent = 'New test';
-    diagnosticPara.textContent = 'Error occurred in recognition: ' + event.error;
+    diagnoPara.textContent = 'Error occurred in recognition: ' + event.error;
   }
   
   recognition.onaudiostart = function(event) {
