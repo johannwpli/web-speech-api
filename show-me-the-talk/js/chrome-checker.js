ff7c5286382b06c220049e557ca0c35ea0c5d90d
paint1024 <paint1024@hotmail.com>
2020-05-14 17:16:33 +0800

    test PWA

--- show-me-the-talk/js/chrome-checker.js
+++ show-me-the-talk/js/chrome-checker.js
@@ -50,3 +50,18 @@ function showInfo() {
 }
 
 infoIcn.addEventListener('click', showInfo);
+
+
+/* PWA by Johann */
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
