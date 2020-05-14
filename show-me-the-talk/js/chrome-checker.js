a2d1180fbb8127ca663dda2153f584aa96a8ab23
paint1024 <paint1024@hotmail.com>
2020-05-14 17:30:07 +0800

    fix PWA

--- show-me-the-talk/js/chrome-checker.js
+++ show-me-the-talk/js/chrome-checker.js
@@ -56,7 +56,7 @@ infoIcn.addEventListener('click', showInfo);
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('service-worker.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }).catch(function(err) {
